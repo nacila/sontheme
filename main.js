3072dbd984fc787c0ba6a319516376d15a6097b3
Can BAYRAM <50000223+nacila@users.noreply.github.com>
2019-08-14 19:27:09 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+new hoverEffect({
+    parent: document.querySelector(".distortion"),
+    intensity:0.2,
+    image1:"../img/prewiew.png",
+    image2:"../img/after.png",
+    displacementImage: "../img/heightMap.png"
+})
