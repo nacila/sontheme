a7eaa53aac2821112c048aa0d17b7a72e605c66a
Can BAYRAM <50000223+nacila@users.noreply.github.com>
2019-08-19 23:14:21 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ new hoverEffect({
     image2:"../img/after.png",
     displacementImage: "../img/heightMap.png"
 })
+
+function openNav() {
+document.getElementById("myNav").style.width = "100%";
+
+}
+
+function closeNav() {
+document.getElementById("myNav").style.width = "0%";
+ }
